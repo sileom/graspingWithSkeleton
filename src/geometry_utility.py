9530bb1acaacf741b16bda33eb96c07631e9c4e5
sileom <64159299+sileom@users.noreply.github.com>
2024-05-20 16:17:41 +0200

    Update geometry_utility.py

--- src/geometry_utility.py
+++ src/geometry_utility.py
@@ -1,22 +1,27 @@
 import numpy as np
+import math as m
+from graspingPoints import *
 
-class GeomUtility:
-
-    #@staticmethod
-    #def planeWith3Points(P):
-    #    points = np.array(P)
-    #    p1 = points[0, 0:3] #np.array([1, 2, 3])
-    #    p2 = points[1, 0:3] #np.array([4, 6, 9])
-    #    p3 = points[2, 0:3] #np.array([12, 11, 9])
-    #    # These two vectors are in the plane
-    #    v1 = p3 - p1
-    #    v2 = p2 - p1
-    #    # the cross product is a vector normal to the plane
-    #    cp = np.cross(v1, v2)
-    #    a, b, c = cp
-    #    # This evaluates a * x3 + b * y3 + c * z3 which equals d
-    #    d = np.dot(cp, p3)
-    #    print('The equation is {0}x + {1}y + {2}z = {3}'.format(a, b, c, d))
+class GeomUtility:    
+    @staticmethod
+    def Rx(theta):
+        return np.array([[1, 0, 0],
+                   [ 0, m.cos(theta),-m.sin(theta)],
+                   [ 0, m.sin(theta), m.cos(theta)]])
+    
+    
+    @staticmethod
+    def Ry(theta):
+        return np.array([[ m.cos(theta), 0, m.sin(theta)],
+                   [0, 1, 0],
+                   [-m.sin(theta), 0, m.cos(theta)]])
+    
+    
+    @staticmethod
+    def Rz(theta):
+        return np.array([[ m.cos(theta), -m.sin(theta), 0 ],
+                   [ m.sin(theta), m.cos(theta) , 0 ],
+                   [0, 0, 1]])
 
 
     @staticmethod
@@ -63,10 +68,10 @@ class GeomUtility:
         p_med[0] = (p1[0]+p2[0])/2
         p_med[1] = (p1[1]+p2[1])/2
         p_med[2] = (p1[2]+p2[2])/2
-        print("punto medio")
-        print(p_med)
+        #print("punto medio")
+        #print(p_med)
         v = p_med - pp #pp - p_med
-        print(v)
+        #print(v)
         g = v / np.linalg.norm(v)
         return g
 
@@ -96,6 +101,8 @@ class GeomUtility:
         R2 = np.array([[1., 0., 0.],
                      [0., 1., 0.],
                      [0., 0., 1.]])
+        
+        x,y,z = [[0., 0., 0.],[0., 0., 0.],[0., 0., 0.]]
 
         if 'back' in detectedBB.classe.lower():
             T[:3,3] = high_p
@@ -103,46 +110,148 @@ class GeomUtility:
             y = normal_vec
             z = lying_vec
             x = np.cross(y,z)
-            print(x)
-            print(y)
-            print(z)
-            T[:3,0] = x
-            T[:3,1] = y
-            T[:3,2] = z
         else:
-            T[:3,3] = keypoints3D[0,:] # -1 is index for last element, -2 penultimo
             # first matrix 
             z = normal_vec
             y = lying_vec
             x = np.cross(y,z)
-            R1[:,0] = x
-            R1[:,1] = y
-            R1[:,2] = z
-            # second matrix 
-            y = -np.array(y)
-            x = np.cross(y,z)
-            R2[:,0] = x
-            R2[:,1] = y
-            R2[:,2] = z
-
-            err1=R_curr.T.dot(R1)
-            err2=R_curr.T.dot(R2)
-
-            asseA1 = GeomUtility.r2asseangolo(R1)
-            asseA2 = GeomUtility.r2asseangolo(R2)
-
-            if asseA1[-1] < asseA2[-1]:
-                T[:3,:3] = R1
-            else:
-                T[:3,:3] = R2
-            #T[:3,0] = x
-            #T[:3,1] = y
-            #T[:3,2] = z
+        R1[:,0] = x
+        R1[:,1] = y
+        R1[:,2] = z
+        # second matrix 
+        y = -np.array(y)
+        x = np.cross(y,z)
+        R2[:,0] = x
+        R2[:,1] = y
+        R2[:,2] = z
+        err1=R_curr.T.dot(R1)
+        err2=R_curr.T.dot(R2)
+        asseA1 = GeomUtility.r2asseangolo(err1)
+        asseA2 = GeomUtility.r2asseangolo(err2)
+        print('DECISIONE ASSE')
+        print('R1')
+        print(R1)
+        print('R2')
+        print(R2)
+        print(' ang1 ---- ang2')
+        print(str(asseA1[-1]) + ' ---- ' + str(asseA2[-1]))
+        if asseA1[-1] < asseA2[-1]:
+            T[:3,:3] = R1
+        else:
+            T[:3,:3] = R2
+        if 'back' in detectedBB.classe.lower():
+            T[:3,3] = high_p
+        else:
+            print("vecchio")
+            print(keypoints3D[0,:])
+            print("nuovo")
+            print(keypoints3D[0,:] +np.array(T[:3,:3]).dot([0,0,-0.0]))
+            T[:3,3] = keypoints3D[0,:] + T[:3,:3].dot([0,0,-0.01])# -1 is index for last element, -2 penultimo
         print(T)
         return T
-        
 
-            
+
+    @staticmethod
+    def getHigherPointInCameraFrame(keypoints,keyIdxes):
+        higherPoint = np.array([10,10,10])
+        higherPointIdx = 0
+        for i in range(keypoints.shape[0]):
+            point = keypoints[i]
+            norm_curr = np.linalg.norm(point)
+            if norm_curr < np.linalg.norm(higherPoint) and i in keyIdxes:
+                higherPoint = point
+                higherPointIdx = i
+        return [higherPoint, higherPointIdx]
+
+    @staticmethod
+    def isVisible(a): #a e' un punto 3D
+        return a[0] != -1 and a[1] != -1 and a[2] != -1
+
+    @staticmethod
+    def getVisibleTripleIdxes(keypoints, obj_name):
+        l = range(0,keypoints.shape[0], 1)
+        comb = combinations(l,3)
+        listaComb = list(comb)
+        dimListaComb = len(listaComb)
+        for i in (range(dimListaComb)):
+            triple = listaComb[i]
+            print(triple)
+            print(triple[0])
+            print("+++++")
+            if 'castiron' in obj_name:
+                if 'back' in obj_name:
+                    esito = not(triple[0]==0) and GeomUtility.isVisible(keypoints[triple[0],:]) and GeomUtility.isVisible(keypoints[triple[1],:]) and GeomUtility.isVisible(keypoints[triple[2],:])
+                else:
+                    esito = GeomUtility.isVisible(keypoints[triple[0],:]) and GeomUtility.isVisible(keypoints[triple[1],:]) and GeomUtility.isVisible(keypoints[triple[2],:])
+            elif 'plastic' in obj_name:
+                if 'back' in obj_name:
+                    esito = (triple[0]!=3 and triple[1]!=3 and triple[2]!=3) and GeomUtility.isVisible(keypoints[triple[0],:]) and GeomUtility.isVisible(keypoints[triple[1],:]) and GeomUtility.isVisible(keypoints[triple[2],:])
+                else:
+                    esito = GeomUtility.isVisible(keypoints[triple[0],:]) and GeomUtility.isVisible(keypoints[triple[1],:]) and GeomUtility.isVisible(keypoints[triple[2],:])
+            elif 'air' in obj_name:
+                esito = (triple[0]!=1 and triple[1]!=1 and triple[2]!=1) and (triple[0]!=2 and triple[1]!=2 and triple[2]!=2) and (triple[0]!=0 and triple[1]!=0 and triple[2]!=0) and GeomUtility.isVisible(keypoints[triple[0],:]) and GeomUtility.isVisible(keypoints[triple[1],:]) and GeomUtility.isVisible(keypoints[triple[2],:])
+            if esito == True:
+                return [triple[0],triple[1],triple[2]]
+        return [-1,-1,-1]
+
+
+    @staticmethod
+    def computeGraspingPointWithTriple(detectedBB, keypoints3D, R_curr):
+        print("[computeGraspingPointWithTriple]")
+        if 'oil' in detectedBB.classe.lower() and 'castiron' in detectedBB.classe.lower():
+            obj_name = "oil_separator_crankcase_castiron"
+        elif 'oil' in detectedBB.classe.lower() and 'plastic' in detectedBB.classe.lower():
+            obj_name = "oil_separator_crankcase_plastic"
+        elif 'air' in detectedBB.classe.lower():
+            obj_name = "air_pipe"
+        
+        objOil = ObjX(detectedBB.classe.lower())
+        [higherKeyPoint, higherKeyPointIdx] = GeomUtility.getHigherPointInCameraFrame(keypoints3D, objOil.getGpIndexes()) # punto più in alto e relativo indice nella lista dei keypoint
+        print("")
+        print(higherKeyPoint)
+        print(higherKeyPointIdx)
+        nominalGraspingPoint = objOil.selectGraspingPoint(higherKeyPointIdx)
+        print(nominalGraspingPoint.getGraspingPoint())
+        if nominalGraspingPoint == -1:
+            print("ERROR: nominalGraspingPoint not found. Abort")
+
+        [ia,ib,ic] = GeomUtility.getVisibleTripleIdxes(keypoints3D, detectedBB.classe.lower()) # indici dei primi keypoint visibili, usare le combinazioni così manteniamo gli indici delle triplette
+        print()
+        print(ia)
+        print(ib)
+        print(ic)
+        print()
+        if ia == -1 and ib == -1 and ic == -1:
+            print("ERROR: Tripla not found. Abort")
+        plane_curr = T_plane(keypoints3D[ia,:],keypoints3D[ib,:],keypoints3D[ic,:]) # --> T_3^c
+        tg_curr = nominalGraspingPoint.getGTranformWithSpecificTripleIdxes(ia,ib,ic)
+        if tg_curr == -1:
+            print("ERROR: Tg not found. Abort")
+
+        T_grasp_c = plane_curr.getT3().dot(tg_curr.getTg()) # GRASPING POINT CAMERA FRAME
+
+
+        if 'castiron' in obj_name:
+            if higherKeyPointIdx == 0:
+                T_grasp_c[0:3,3] = T_grasp_c[0:3,3] + T_grasp_c[0:3,0:3].dot([0,0,0.0015])
+            else:
+                T_grasp_c[0:3,3] = T_grasp_c[0:3,3] + T_grasp_c[0:3,0:3].dot([0,0,0.003])
+        elif 'plastic' in obj_name:
+            if higherKeyPointIdx == 3:
+                T_grasp_c[0:3,3] = T_grasp_c[0:3,3] + T_grasp_c[0:3,0:3].dot([0,0,0.005])
+        elif 'air' in obj_name:
+            if higherKeyPointIdx == 3:
+                T_grasp_c[0:3,3] = T_grasp_c[0:3,3] + T_grasp_c[0:3,0:3].dot([0,0,0.005])
+
+        return T_grasp_c
+        
+    
+    @staticmethod
+    def isIn(bb, pixel):
+        if bb.x <= pixel[0] and pixel[0] <= (bb.x + bb.w):
+            if bb.y <= pixel[1] and pixel[1] <= (bb.y + bb.h):
+                return True
+        return False       
 
 
 
@@ -221,6 +330,7 @@ class GeomUtility:
             q[0] =  -p4
         return q
     
+
     @staticmethod
     def r2asseangolo(R):
         # r_x, r_y, r_z, theta
@@ -287,51 +397,20 @@ class GeomUtility:
                 vi = 0
             if ui < 0:
                 ui = 0
-            if vf > depth_array.shape[1]:
-                vf = depth_array.shape[1]
-            if uf > depth_array.shape[0]:
-                uf = depth_array.shape[0]
+            if vf > depth_array.shape[0]:
+                vf = depth_array.shape[0]
+            if uf > depth_array.shape[1]:
+                uf = depth_array.shape[1]
 
 
             if d1 != 0.:
                 d = d1
             else:
                 intornoMIN = np.array(depth_array[vi:vf, ui:uf])
-                print('DIMENSIONE INTORNO')
-                print(intornoMIN.shape)
-                print("vi: " + str(vi) + " vf: " + str(vf) + " ui: " + str(ui) + " uf: " + str(uf))
                 nonZeroValuesMIN = intornoMIN[np.nonzero(intornoMIN)]
-                #print("non zero min")
-                #print(nonZeroValuesMIN)
                 d = np.min(nonZeroValuesMIN)
-                print('d3')
-                print(d)
-
-
             r = 0
             d2 = 0.
-
-            #if(v-60 > 0 and u-60 > 0):
-            #    if(v+60 < 480 and u+60 < 640):
-            #        intornoMIN = depth_array[v-60:v+60, u-60:u+60]
-            #        nonZeroValuesMIN = intornoMIN[np.nonzero(intornoMIN)]
-            #        d3 = np.min(nonZeroValuesMIN)
-            #        print('d3')
-            #        print(d3)
-            
-
-            #nonZeroValues = depth_array[v,u] 
-            #while d2 == 0.:
-            #    if not np.all((nonZeroValues == 0)):
-            #        d2 = np.mean(nonZeroValues)
-            #        print('d2')
-            #        print(d2)
-            #    else:
-            #        r = r + 1
-            #        intorno = depth_array[v-r:v+r, u-r:u+r]
-            #        nonZeroValues = intorno[np.nonzero(intorno)]
-            #        print(intorno)
-            
             x_ = (u - K[0,2])/K[0,0]
             y_ = (v - K[1,2])/K[1,1]
             z = (float(np.array(d))) / 1000
@@ -341,15 +420,53 @@ class GeomUtility:
         else: 
             return np.array([-1, -1, -1])
 
+
     @staticmethod
-    def get_matrix(normal, y_w):
-        R = np.array([[-0.094378, 0.995485, -0.009164],
-                    [0.995355, 0.094186, -0.019453],
-                    [-0.018502, -0.010957, -0.999769]])
-        v_ = R.dot(normal)
-        x = np.cross(normal,y_w)
-        #x = np.cross(np.array([1, 0, 0]), normal)
-        x = x/np.linalg.norm(x)
-        y = np.cross(normal, x)
-        y = y/np.linalg.norm(y)
-        return np.array([x, y, normal]).T 
+    def computeNewLookPose(po, Ae_curr, angleForLook):
+        pRob = Ae_curr[:3,3]
+
+        center = np.array([po[0], po[1], pRob[2]])
+        r = np.linalg.norm(center - pRob)
+
+        angle_curr = np.arcsin((pRob[1]-center[1])/r)
+        angle_curr2 = np.arcsin((center[1]-pRob[1])/r)
+        angle_curr3 = np.arcsin((center[0]-pRob[0])/r)
+
+        px_new = r*np.cos(angle_curr+angleForLook) + center[0]
+        py_new = r*np.sin(angle_curr+angleForLook) + center[1]
+        pz_new = center[2]
+        p_new = np.array([px_new, py_new, pz_new])
+        x = (center-p_new)/r
+        z = (po - center) / np.linalg.norm(po-center)
+        y = np.cross(z,x)
+
+        T= np.array([[1., 0., 0., 0.],
+                     [0., 1., 0., 0.],
+                     [0., 0., 1., 0.],
+                     [0., 0., 0., 1.]])
+        T[:3,0] = x
+        T[:3,1] = y
+        T[:3,2] = z
+        T[0,3] = px_new
+        T[1,3] = py_new
+        T[2,3] = pz_new
+        T_new = np.array([[-0.795977, 0.603738, 0.0436101, -0.112803],
+                            [0.597776, 0.795358, -0.100255, 0.467724],
+                            [-0.0952133, -0.0537317, -0.994006, 0.430012],
+                            [0, 0, 0, 1]])
+        return T_new
+        #return T
+
+    
+    @staticmethod
+    def getCenterBB(detection):
+        xr = detection.x + detection.w
+        yb = detection.y + detection.h
+        a = (detection.x + xr)/2
+        b = (detection.y + yb)/2
+        center = [int(a), int(b)]
+        return center
+        
+
+        
+
