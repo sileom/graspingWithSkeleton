baf96fdfe13b74c6aeb378ef8af058c26661bea1
sileom <64159299+sileom@users.noreply.github.com>
2024-05-20 16:18:28 +0200

    Update runDNN.sh

--- runDNN.sh
+++ runDNN.sh
@@ -1,3 +1,11 @@
 cd ~/object_skeleton
 source skeleton-venv3_6/bin/activate
-python lightweight-human-pose-estimation.pytorch/demo_iros.py --checkpoint-path checkpoints_mobilenet_v3_384/checkpoint_iter_11000.pth --images Grigio_color_1772.png
+#
+# OIL_SEPARATOR_CAST_IRON
+#python lightweight-human-pose-estimation.pytorch/demo_iros.py --checkpoint-path Model_oil_castIron/checkpoint_iter_12500.pth --images data/rgb_01.png
+#
+# AIR_PIPE 
+#python demo.py --checkpoint-path Model_airPipe/checkpoint_iter_13000.pth --images data/rgb_01.png
+#
+# OIL_SEPARATOR_PLASTIC
+python demo.py --checkpoint-path Model_oil_plastic/checkpoint_iter_30000.pth --images data/rgb_01.png
